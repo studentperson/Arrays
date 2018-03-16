dd1efe33bfbeddadcf9d0b74d526e991607cf557
Norman Mamani <31675305+studentperson@users.noreply.github.com>
2018-03-16 05:28:47 -0700

    Delete catTest00.bash

--- Arrays-master-template/shellTests/catTest00.bash
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# This is a comment
-#The main point is to test concatenation
-	#This is to make it easier to sort organize files
-for number in {1..10..2}
-do
-	echo "Hello"$number
-done
