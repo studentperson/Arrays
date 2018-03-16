f82b1597333cb1df998b174b9f327e7d711d7077
Norman Mamani <31675305+studentperson@users.noreply.github.com>
2018-03-16 05:30:20 -0700

    Delete saveFileParamTest00.bash

--- Arrays-master-template/shellTests/saveFileParamTest00.bash
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# This is a comment
-#This is to test saving the results of a shell script to a file
-#make script executable with chmod
-#./executableName parameters... > saveFileName
-./executeParamTest00.bash 0 0 10 > testresults01.txt
